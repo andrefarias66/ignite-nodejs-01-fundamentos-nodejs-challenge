6f4235f6b2fc6f6df113ba2dd2fa07d7ef16398b
Andre Farias <andrefarias66@gmail.com>
2024-02-29 18:57:50 -0300

    trata parametros enviados no PUT e POST

--- src/routes.js
+++ src/routes.js
@@ -26,6 +26,18 @@ export const routes = [
     handler: (req, res) => {
       const { title, description } = req.body
 
+      if (!title) {
+        return res.writeHead(400).end(
+          JSON.stringify({ message: 'title is required' }),
+        )
+      }
+
+      if (!description) {
+        return res.writeHead(400).end(
+          JSON.stringify({message: 'description is required' })
+        )
+      }
+
       const user = {
         id: randomUUID(),
         title,
@@ -63,6 +75,12 @@ export const routes = [
     handler: (req, res) => {
       const { id } = req.params
       const { title, description } = req.body
+
+      if (!title && !description) {
+        return res.writeHead(400).end(
+          JSON.stringify({ message: 'title or description are required' })
+        )
+      }      
 
       const [task] = database.select('tasks', { id })
 
